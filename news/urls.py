3c7d2dfa3900188a1d1d7b450b58242ac502f1d9
yuryatlant <yuryatlant@gmail.com>
2023-04-08 18:06:10 +0300

    modified:   db.sqlite3 	modified:   news/__pycache__/urls.cpython-38.pyc 	modified:   news/urls.py 	modified:   templates/news.html

--- news/urls.py
+++ news/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', NewsList.as_view()), # т.к. сам по себе это класс, то нам надо представить этот класс в виде view. Для этого вызываем метод as_view
     path('<int:pk>', NewsDetail.as_view(), name='new'),
     path('search', NewsSearch.as_view()),
-    path('new_create/', NewsCreateView.as_view(), name='new_create'),
-    path('new_delete/', NewsDeleteView.as_view(), name='new_delete'),
-    path('new_update/', NewsUpdateView.as_view(), name='new_update'),
+    path('new_create/<int:pk>/', NewsCreateView.as_view(), name='new_create'),
+    path('new_delete/<int:pk>/', NewsDeleteView.as_view(), name='new_delete'),
+    path('new_update/<int:pk>/', NewsUpdateView.as_view(), name='new_update'),
 ]
