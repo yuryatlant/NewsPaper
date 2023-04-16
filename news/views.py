b76d632b069f32a6c9c044b77a7bb75732fa4c7e
yuryatlant <yuryatlant@gmail.com>
2023-04-16 22:35:18 +0300

    modified:   news/views.py

--- news/views.py
+++ news/views.py
@@ -61,7 +61,7 @@ class NewsCreateView(CreateView):
     form_class = NewsForm
 
 
-class NewsUpdateView(UpdateView,LoginRequiredMixin):
+class NewsUpdateView(LoginRequiredMixin,UpdateView):
     template_name = 'new_create.html'
     form_class = NewsForm
     # метод get_object мы используем вместо queryset, чтобы получить информацию об объекте который мы собираемся редактировать
